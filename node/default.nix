962a3a76deda93a8688be50f3782d1fa3a2bd366
Sebastian Rakel <sebastian@devunit.eu>
2025-07-06 20:44:56 +0200

    Update node env

--- node/default.nix
+++ node/default.nix
@@ -1,11 +1,19 @@
 { pkgs, ... }: {
-  devShells.node = (pkgs.buildFHSEnv {
-    name = "nodejs";
-    targetPkgs = pkgs: [ pkgs.gcc pkgs.libtool pkgs.nodejs_22 pkgs.yarn ];
-    runScript = "zsh";
-    profile = ''
+  devShells.node = (let
+    fhs = pkgs.buildFHSEnv {
+      name = "node-env";
+      targetPkgs = pkgs: with pkgs; [ gcc libtool nodejs_22 yarn ];
+      runScript = "bash";
+      profile = ''
+        export PATH=$(yarn global bin):$PATH  
+      '';
+    };
+  in pkgs.stdenv.mkDerivation {
+    name = "node-shell";
+    nativeBuildInputs = [ fhs ];
+    shellHook = ''
+      exec node-env
       yarn global add @quasar/cli
-      export PATH=$(yarn global bin):$PATH    
     '';
-  }).env;
+  });
 }
