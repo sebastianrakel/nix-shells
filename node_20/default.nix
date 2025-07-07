09cd14eafc6ddc33006410616c29216c5cbf9cce
Sebastian Rakel <sebastian.rakel@service-erp.de>
2025-07-07 09:18:48 +0200

    Update node 20 shell

--- node_20/default.nix
+++ node_20/default.nix
@@ -1,20 +1,20 @@
-{ pkgs, ... }:
-let
-  packages = [ pkgs.gcc pkgs.libtool pkgs.nodejs_20 pkgs.yarn ];
-  nodeFhs = (pkgs.buildFHSEnv {
-    name = "nodejs dev env";
-    targetPkgs = packages;
-    runScript = "bash";
-  });
-in {
-  devShells.node_20 = pkgs.mkShell {
-    inputsFrom = [ nodeFhs ];
-    packages = packages;
-
+{ pkgs, ... }: {
+  devShells.node_20 = (let
+    fhs = pkgs.buildFHSEnv {
+      name = "node-20-env";
+      targetPkgs = pkgs: with pkgs; [ gcc libtool nodejs_22 yarn ];
+      runScript = "bash";
+      profile = ''
+        export PATH=$(yarn global bin):$PATH  
+      '';
+    };
+  in pkgs.stdenv.mkDerivation {
+    name = "node-20-shell";
+    nativeBuildInputs = [ fhs ];
     shellHook = ''
-      PATH=$(yarn global bin):$PATH
+      exec node-20-env
       yarn global add @quasar/cli
+      yarn global add @forge/cli
     '';
-  };
+  });
 }
-
